dead536a1f69cc38c667f3c27dded482aad7dcd6
Pricklen <igorsamodumskij@gmail.com>
2026-03-26 18:33:19 +0300

    kiln menu bugfix

--- src/main/java/com/pricklen/machines/screen/KilnMenu.java
+++ src/main/java/com/pricklen/machines/screen/KilnMenu.java
@@ -31,7 +31,7 @@ public class KilnMenu extends AbstractContainerMenu {
         this.level = inv.player.level();
         this.data = data;
         addPlayerHotbar(inv);
-        addPlayerHotbar(inv);
+        addPlayerInventory(inv);
         this.blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(iItemHandler -> {
             this.addSlot(new SlotItemHandler(iItemHandler, 0, 80, 11));
             this.addSlot(new SlotItemHandler(iItemHandler, 1, 80, 59));
